f779b7bf232d2cf3ce35886566105dd041c37bd9
Eunjilee419 <kejlee001@gmail.com>
2025-05-22 20:02:52 +0900

    Update testSaju.js

--- api/testSaju.js
+++ api/testSaju.js
@@ -1,16 +1,13 @@
+
 const { getSajuFromDate, getLackingElements } = require('./sajuUtils.js');
 
-async function test() {
-  try {
-    const dob = '1991-08-10';
-    const saju = getSajuFromDate(dob);
-    console.log('사주:', saju);
+function test() {
+  const dob = '1991-08-10';
+  const saju = getSajuFromDate(dob);
+  console.log('사주:', saju);
 
-    const lacking = getLackingElements(saju);
-    console.log('부족 오행:', lacking);
-  } catch (e) {
-    console.error('에러 발생:', e);
-  }
+  const lacking = getLackingElements(saju);
+  console.log('부족 오행:', lacking);
 }
 
 test();
